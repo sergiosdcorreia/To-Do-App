7d1c32310e4e8485703898e6a9525c6998d75bbe
sergiosdcorreia <sergiosdcorreia@gmail.com>
2018-12-13 12:04:56 +0000

    To Do App first commit

---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ToDo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
